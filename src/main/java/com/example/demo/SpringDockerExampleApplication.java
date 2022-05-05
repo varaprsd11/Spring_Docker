21716eef583a0a2cf5f395923508e5ece72dbd84
varaprsd11 <varaprsd11@gmail.com>
2022-05-05 14:03:42 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringDockerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerExampleApplication.class, args);
+	}
+
+}
